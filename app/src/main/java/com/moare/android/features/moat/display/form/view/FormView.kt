abdaaeef9eedcea9c585ba20aaf116b5910e99c2
최지혜 <jhchoi@joara.com>
2025-09-26 11:36:07 +0900

    feature: Create FormView and FormViewModel

---
+++ app/src/main/java/com/moare/android/features/moat/display/form/view/FormView.kt
@@ -0,0 +1,94 @@
+package com.moare.android.features.moat.display.form.view
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.moare.android.features.moat.display.form.viewmodel.FormIntent
+import com.moare.android.features.moat.display.form.viewmodel.FormViewModel
+import com.moare.android.features.moat.display.moat.viewmodel.MoatIntent
+import com.moare.android.features.moat.display.moat.viewmodel.MoatViewModel
+
+@Composable
+fun FormView(
+    moatViewModel: MoatViewModel = hiltViewModel(),
+) {
+    var text by rememberSaveable { mutableStateOf("") }
+    
+    val hashtagList = listOf("#축구", "#농구", "#야구", "#테니스")
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(horizontal = 6.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = "모트 작성",
+            )
+
+            Text(
+                text = "첫번째 줄은 메인에서 주제(썸네일?)로 표시됩니다.",
+                fontSize = 12.sp,
+                color = Color.Gray
+            )
+
+            OutlinedTextField(
+                value = text,
+                onValueChange = { newValue ->
+                    text = newValue
+                },
+                modifier = Modifier.fillMaxWidth()
+                    .height(100.dp)
+            )
+
+            LazyRow(
+                modifier = Modifier.fillMaxWidth()
+                    .align(Alignment.Start)
+            ) {
+                items(hashtagList.size) { index ->
+                    Text(
+                        text = hashtagList[index],
+                        modifier = Modifier.padding(horizontal = 4.dp)
+                            .clickable(true, onClick = {})
+                    )
+                }
+            }
+
+            Button(
+                onClick = { moatViewModel.send(MoatIntent.CreateMoat(text)) }
+            ) {
+                Text(
+                    text = "작성하기",
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun FormViewPreview() {
+//    FormView()
+}
